7342b259ccf85e5668e822305220d58d3616ddd6
adamkoval <akoval@LAPTOP-M692C3QH.localdomain>
2019-12-28 13:33:15 +0700

    Add bashenv to read_envfile() function.

--- mcm/func.py
+++ mcm/func.py
@@ -7,4 +7,5 @@ def read_envfile(envfile):
     with open(envfile, 'r') as f:
         lines = [line for line in f.readlines() if line[0] is not '#']
         pyenv = lines[0].split()[2]
-    return pyenv
+        bashenv = lines[1].split()[2]
+    return pyenv, bashenv
