f3815c9aac44ab7c03d22a9243bc8d8f33a3e788
adamkoval <akoval@LAPTOP-M692C3QH.localdomain>
2020-01-18 01:51:12 +0000

    Edit name of individual compiled mercury and element instances.

--- mcm/check_mercury.sh
+++ mcm/check_mercury.sh
@@ -2,13 +2,13 @@
 
 pno=$1
 
-(cd mercury_$pno/; ./mercury6_2_$pno) &
-while pgrep -x mercury6_2_$pno > /dev/null
+(cd mercury_$pno/; ./mercury_$pno.exe) &
+while pgrep -x mercury_$pno.exe > /dev/null
 do
     if grep -e was\ hit\ by -e ejected\ at -e collided\ with\ the\ central\ body mercury_$pno/info.out
     then
-        kill `pidof mercury6_2_$pno`
+        kill `pidof mercury_$pno.exe`
     fi
 done
 
-(cd mercury_$pno/; ./element6_2)
+(cd mercury_$pno/; ./element_$pno.exe)
