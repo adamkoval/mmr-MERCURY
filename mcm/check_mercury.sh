e1ad19ee927d285b1e64b5c91757ecd5445ecda0
adamkoval <akoval@LAPTOP-M692C3QH.localdomain>
2020-01-18 02:01:09 +0000

    Change pidof to pgrep.

--- mcm/check_mercury.sh
+++ mcm/check_mercury.sh
@@ -7,7 +7,7 @@ while pgrep -x mercury_$pno.exe > /dev/null
 do
     if grep -e was\ hit\ by -e ejected\ at -e collided\ with\ the\ central\ body mercury_$pno/info.out
     then
-        kill `pidof mercury_$pno.exe`
+        kill `pgrep mercury_$pno.exe`
     fi
 done
 
