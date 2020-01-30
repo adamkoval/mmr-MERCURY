b8636b8f8b64adac0663e690c9bdb9f011588079
adamkoval <akoval@LAPTOP-M692C3QH.localdomain>
2020-01-30 00:27:21 +0000

    Add test for interactive point picker for mu1-mu2 plot.

--- analysis/test_simplot.py
+++ analysis/test_simplot.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 
 import func as fn
 
-resonance = '53'
+resonance = '21'
 completed_path = '../completed'
 sim_results = fn.MM_sim_results(completed_path, resonance)
 
 fig, ax, boundary = fn.stability_fig_setup(resonance)
-fn.plot_sims(sim_results, resonance, fig, ax)
+fn.plot_sims(sim_results, fig, ax)
+fn.plot_sims_timeevol(sim_results, fig)
 plt.show()
