d8edf345e9731416cf9a8c479cb0e3f89fb3fee5
adamkoval <akoval@LAPTOP-M692C3QH.localdomain>
2020-02-09 14:53:47 +0000

    Test script.

--- analysis/test_simplot.py
+++ analysis/test_simplot.py
@@ -2,11 +2,13 @@ import matplotlib.pyplot as plt
 
 import func as fn
 
+#resonance = '32'
+#completed_path = '../../../sim_results/incl=10_completed'
 resonance = '21'
 completed_path = '../completed'
 sim_results = fn.MM_sim_results(completed_path, resonance)
 
 fig, ax, boundary = fn.stability_fig_setup(resonance)
 fn.plot_sims(sim_results, fig, ax)
-coords = fn.interactive_mu1mu2(resonance, sim_results, fig)
+coords = fn.interactive_mu1mu2(completed_path, resonance, sim_results, fig)
 plt.show()
