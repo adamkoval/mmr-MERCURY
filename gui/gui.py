59960b8aba3a6d274ffe983286dd789af16fb388
adamkoval <akoval@LAPTOP-M692C3QH.localdomain>
2020-02-10 19:05:00 +0000

    Add popup text window destined for editing param.in file.

--- gui/gui.py
+++ gui/gui.py
@@ -1,52 +1,5 @@
 import tkinter as tk
 
-class MainApp(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
-
-        mainframe = tk.Frame(self)
-        mainframe.pack(side="top", fill="both", expand=True)
-        mainframe.grid_rowconfigure(0, weight=1)
-        mainframe.grid_columnconfigure(0, weight=1)
-
-        self.pages = {}
-        for _page in (HomePage, SimPage, AnalysisPage, SetupPage):
-            page_name = _page.__name__
-            page = _page(parent=mainframe, controller=self)
-            self.pages[page_name] = page
-            page.grid(row=0, column=0, sticky="NESW")
-
-        self.show_page("HomePage")
-
-    def show_page(self, page_name):
-        print("Raising page {}".format(page_name)) # DEBUG
-        page = self.pages[page_name]
-        page.tkraise()
-
-
-class SetupPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        GenericPage(self, controller, "Setup")
-
-
-class AnalysisPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        GenericPage(self, controller, "Analysis")
-
-
-class SimPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        GenericPage(self, controller, "Simulation")
-
-
-class HomePage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        GenericPage(self, controller, "Home")
-
 
 class GenericPage(tk.Frame):
     def __init__(self, parent, controller, page_name):
@@ -77,6 +30,100 @@ class NavBar(tk.Frame):
             dest = buttons[text]
             NavButton(self, controller, text, dest, i)
         self.pack()
+
+
+
+
+class MainApp(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        mainframe = tk.Frame(self)
+        mainframe.pack(side="top", fill="both", expand=True)
+        mainframe.grid_rowconfigure(0, weight=1)
+        mainframe.grid_columnconfigure(0, weight=1)
+
+        self.pages = {}
+        for _page in (HomePage, SimPage, AnalysisPage, SetupPage):
+            page_name = _page.__name__
+            page = _page(parent=mainframe, controller=self)
+            self.pages[page_name] = page
+            page.grid(row=0, column=0, sticky="NESW")
+
+        self.show_page("HomePage")
+
+    def show_page(self, page_name):
+        page = self.pages[page_name]
+        page.tkraise()
+
+
+class HomePage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        GenericPage(self, controller, "Home")
+
+
+class SetupPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        GenericPage(self, controller, "Setup")
+
+        paramin = "text.txt"
+        paramin_button = tk.Button(self, text="Edit param.in",
+                command=lambda: Popup(self, paramin))
+        paramin_button.pack()
+
+
+#class TextWindow2(tk.Toplevel):
+#    def __init__(self, parent, file):
+#        tk.Toplevel.__init__(self, parent)
+#        text_window = TextWindow(self, file)
+#        save_button = tk.Button(self, text="Save changes",
+#                command=lambda: text_window.save_file())
+#        save_button.pack()
+#
+#    def TextBox(self, parent, file):
+#        textbox
+
+
+class Popup(tk.Toplevel):
+    def __init__(self, parent, file):
+        tk.Toplevel.__init__(self, parent)
+        text_window = TextWindow(self, file)
+        save_button = tk.Button(self, text="Save changes",
+                command=lambda: text_window.save_file())
+        save_button.pack()
+
+
+class TextWindow(tk.Frame):
+    def __init__(self, parent, file):
+        tk.Frame.__init__(self, parent)
+        self.file = file
+        self.parent = parent
+
+        text = open(self.file, 'r').read()
+        self.textbox = tk.Text(self.parent)
+        self.textbox.pack()
+        self.textbox.insert(1.0, text)
+
+    def save_file(self):
+        text = self.textbox.get(1.0, tk.END)
+        f = open(self.file, 'w')
+        f.write(text)
+        f.close()
+        self.parent.destroy()
+
+
+class AnalysisPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        GenericPage(self, controller, "Analysis")
+
+
+class SimPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        GenericPage(self, controller, "Simulation")
 
 
 if __name__=="__main__":
