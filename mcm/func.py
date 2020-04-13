632fb6374f4a0cec05d6f8fb4fa2ab3bc12c5e7f
adamkoval <akoval@LAPTOP-M692C3QH.localdomain>
2020-04-13 22:20:07 +0100

    Add function to set outer planet place.

--- mcm/func.py
+++ mcm/func.py
@@ -56,6 +56,37 @@ def disp(res_float, fraction, direction=''):
     return disp
 
 
+def set_outer(a_i, res_float, fraction, direction=''):
+    """
+    For the outer planet, sets initial displacement outside of
+    or migration distance inside of the resonance under
+    consideration.
+    In:
+        > a_i - (float) semi-major axis of inner planet
+        > res_float - (float) fractional value of resonance
+        under consideration, i.e., 2/1=2.0, 5/3=1.66667, and
+        so on
+        > fraction - (float) fraction of distance
+        between neighboring resonances by which to
+        displace the outer planet
+        > direction - (str) 'in' or 'out', depending on
+        whether the user sets the initial displacement or
+        the final displacement inside of the resonance
+    Out:
+        > a_o - (float) semi-major axis of outer planet.
+    """
+    res_floats = [9/7, 4/3, 7/5, 3/2, 5/3, 2/1, 3/1]
+    for i in range(1, len(res_floats)-1):
+        if res_floats[i] == res_float:
+            if direction == 'out':
+                disp = fraction * (res_floats[i+1] - res_floats[i])
+                a_o = (res_float + disp)**(2/3) * a_i
+            elif direction == 'in':
+                disp = fraction * (res_floats[i] - res_floats[i-1])
+                a_o = (res_float - disp)**(2/3) * a_i
+    return a_o
+
+
 def count_completed(res_str):
     """
     Counts number of files present in the 'completed' directory of
